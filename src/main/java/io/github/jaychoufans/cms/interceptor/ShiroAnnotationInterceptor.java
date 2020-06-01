3ba21076799792776d8b10a55e15f68dd06d337a
ly <641921791@qq.com>
2020-06-02 00:32:21 +0800

    Commit error page

--- src/main/java/io/github/jaychoufans/cms/interceptor/ShiroAnnotationInterceptor.java
+++ src/main/java/io/github/jaychoufans/cms/interceptor/ShiroAnnotationInterceptor.java
@@ -5,6 +5,7 @@ import io.github.jaychoufans.cms.utils.WebUtils;
 import org.apache.shiro.authz.annotation.RequiresUser;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,8 +13,12 @@ import java.lang.reflect.Method;
 
 public class ShiroAnnotationInterceptor implements HandlerInterceptor {
 
+	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		if (handler instanceof ResourceHttpRequestHandler) {
+			return true;
+		}
 		HandlerMethod handlerMethod = (HandlerMethod) handler;
 		Method method = handlerMethod.getMethod();
 		if (method.isAnnotationPresent(RequiresUser.class)) {
